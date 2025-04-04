c2c78ee067d2d327466e0fec8a131a8c763505f5
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-04-04 18:56:11 +0900

    Del: 필요없는 코드 전부제거

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,8 +7,3 @@ import App from './App';
 test('1 is 1', () => {
   expect(1).toBe(1);
 });
-
-test('render', async () => {
-  render(<App />);
-  expect(screen.getByText('o2pluss template')).toBeInTheDocument();
-});
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,28 +1,5 @@
-import { Button } from '@o2pluss/o2pluss-design-system';
 import React from 'react';
 
 export default function Page() {
-  const [count, setCount] = React.useState(0);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
-  const onClickCountDown = () => {
-    setCount(count - 1);
-  };
-
-  return (
-    <div>
-      <h1>o2pluss template</h1>
-      count: {count}
-      <div className="flex gap-sm">
-        <Button size="sm" onClick={onClickCountUp}>
-          count up
-        </Button>
-        <Button size="sm" onClick={onClickCountDown}>
-          count down
-        </Button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
