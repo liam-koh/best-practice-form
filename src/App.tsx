417f5dc343a4ebec49f01615c08349e635a3fc3b
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-04-04 18:46:51 +0900

    Chore: project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AppLayout from '@/components/AppLayout';
+import Router from './router/router';
+
+function App() {
+  return (
+    <AppLayout>
+      <Router />
+    </AppLayout>
+  );
+}
+
+export default App;
