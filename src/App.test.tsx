49ce8ecc6b3cb3345b6fa023c8cfb28ca40c857a
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-04-06 23:30:25 +0900

    PR: initial project setup (#1)

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+test('1 is 1', () => {
+  expect(1).toBe(1);
+});
