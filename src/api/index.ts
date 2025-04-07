437e37a6f674dd583a94c1041016b1282184d5f8
liam-koh <144200505+liam-koh@users.noreply.github.com>
2025-04-07 19:19:59 +0900

    Feat: Form 기능 구현

---
+++ src/api/index.ts
@@ -0,0 +1,68 @@
+interface TFetchPriceAPIRequest {
+  item: string;
+  senderAddress: string;
+  receiverAddress: string;
+}
+
+/**
+ * 물품 종류 호출 API
+ */
+export const fetchItemAPI = async () => {
+  // Mock API call
+  return new Promise<{ items: string[] }>((resolve) => {
+    setTimeout(() => {
+      resolve({ items: ['사과', '바나나', '쌀', '보리'] });
+    }, 1000);
+  });
+};
+
+/**
+ * 입력한 정보 토대로 가격 조회 API
+ */
+export const fetchPriceAPI = async (options: TFetchPriceAPIRequest) => {
+  // Mock API call
+  return new Promise<{ price: number }>((resolve) => {
+    setTimeout(() => {
+      resolve({ price: Math.floor(Math.random() * 10000) });
+    }, 1000);
+  });
+};
+
+/**
+ * 저장한 form 데이터로 배달 접수 API
+ */
+export const submitFormAPI = async (data: any) => {
+  // Mock API call
+  return new Promise<{ success: boolean }>((resolve) => {
+    setTimeout(() => {
+      // localStorage에 저장
+      localStorage.setItem('deliveryForm', JSON.stringify(data));
+    }, 1000);
+  });
+};
+
+/**
+ * 저장된 배송 데이터 로드 API
+ */
+export const fetchDeliveryListAPI = async () => {
+  // Mock API call
+  return new Promise<{ data: any }>((resolve) => {
+    setTimeout(() => {
+      const rawData = localStorage.getItem('deliveryForm');
+      const rawForm = rawData ? JSON.parse(rawData) : null;
+      resolve({ data: rawForm });
+    }, 1000);
+  });
+};
+
+/**
+ * 배송 접수 submit API
+ */
+export const submitDeliveryAPI = async (data: any) => {
+  // Mock API call
+  return new Promise<{ success: boolean }>((resolve) => {
+    setTimeout(() => {
+      resolve({ success: true });
+    }, 1000);
+  });
+};
--- src/components/DeliveryFormApp.tsx
+++ src/components/DeliveryFormApp.tsx
@@ -20,6 +20,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { fetchDeliveryListAPI, fetchPriceAPI, submitDeliveryAPI } from '@/api';
+import { useQuery } from '@tanstack/react-query';
 
 const DeliveryFormSchema = z.object({
   // string or null
@@ -36,7 +38,7 @@ const DeliveryFormSchema = z.object({
   }),
 });
 
-type TForm = z.infer<typeof DeliveryFormSchema>;
+export type MDeliveryForm = z.infer<typeof DeliveryFormSchema>;
 
 const mockItems = [
   { category: '과일류', options: ['사과', '바나나'] },
@@ -55,120 +57,149 @@ export default function DeliveryFormApp() {
   });
 
   const { register, watch, setValue, handleSubmit, reset, formState } = methods;
-  const values = watch();
 
-  const [price, setPrice] = useState<number | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
   const [submitSuccess, setSubmitSuccess] = useState(true);
 
-  // 가격 계산 API 모의
-  const fetchPrice = () => {
-    const totalQty = values.items.reduce(
-      (sum: number, item: any) => sum + item.quantity,
-      0,
-    );
-    setPrice(totalQty * 1000);
-  };
+  const { data: estimatedPrice, isLoading } = useQuery({
+    queryKey: [
+      'price',
+      watch('item'),
+      watch('sender.address'),
+      watch('receiver.address'),
+    ],
+    queryFn: async () => {
+      const response = await fetchPriceAPI({
+        item: watch('item'),
+        senderAddress: watch('sender.address'),
+        receiverAddress: watch('receiver.address'),
+      });
+      console.log('response', response);
+      return response.price;
+    },
+    enabled:
+      !!watch('item') &&
+      !!watch('sender.address') &&
+      !!watch('receiver.address'),
+    placeholderData: (prev) => prev,
+  });
+
+  console.log(
+    watch('item'),
+    watch('sender.address'),
+    watch('receiver.address'),
+  );
 
   // 접수 API 모의
-  const submitForm = (data: any) => {
-    const success = Math.random() > 0.2; // 80% 확률 성공
-    setSubmitSuccess(success);
-    setModalOpen(true);
+  const submitForm = async (data: any) => {
+    await submitDeliveryAPI(data);
   };
 
-  const saveTemp = () => {
-    localStorage.setItem('deliveryForm', JSON.stringify(values));
+  const onLoadSavedData = async () => {
+    const savedData = await fetchDeliveryListAPI();
+    if (savedData.data) {
+      reset(savedData.data);
+    }
   };
 
-  const loadTemp = () => {
-    const data = localStorage.getItem('deliveryForm');
-    if (data) reset(JSON.parse(data));
+  const onSaveData = async () => {
+    const savedData = await fetchDeliveryListAPI();
+    if (savedData.data) {
+      reset(savedData.data);
+    }
   };
-
-  useEffect(() => {
-    fetchPrice();
-  }, [values.items]);
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={handleSubmit(submitForm)}
-        className="w-full flex justify-start items-start flex-col gap-4 p-4"
-      >
-        <div className='flex flex-col gap-2'>
-          <h2 className="font-semibold">
-            배송 물품 선택
-          </h2>
-          <div className="w-1/2">
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="배송 물품 선택" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>배송 물품 선택</SelectLabel>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+    <main className="w-screen h-screen">
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit(submitForm)}
+          className="w-full flex justify-start items-start flex-col gap-4 p-4"
+        >
+          <div className="flex flex-col gap-2">
+            <h2 className="font-semibold">배송 물품 선택</h2>
+            <div className="w-1/2">
+              <Select
+                value={watch('item')}
+                onValueChange={(value) => setValue('item', value)}
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="배송 물품 선택" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>배송 물품 선택</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-        </div>
-
-        {(['sender', 'receiver'] as const).map((type) => (
-          <div key={type}>
-            <h2 className="font-semibold">
-              {type === 'sender' ? '출발지 정보' : '도착지 정보'}
-            </h2>
-            <Input
-              {...register(`${type}.name`)}
-              placeholder="이름"
-              className="mt-2"
-            />
-            <Input
-              {...register(`${type}.phone`)}
-              placeholder="전화번호"
-              className="mt-2"
-            />
-            <Input
-              {...register(`${type}.address`)}
-              placeholder="주소"
-              className="mt-2"
-            />
-          </div>
-        ))}
-
-        <div>
-          <p className="text-lg font-bold">
-            예상 가격: {price ? `${price.toLocaleString()}원` : '-'}
-          </p>
-        </div>
-
-        <div className="flex gap-2">
-          <Button type="submit" size="default" disabled={!formState.isValid}>
-            접수하기
-          </Button>
-        </div>
-      </form>
-
-      <Dialog open={modalOpen} onOpenChange={setModalOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>
-              {submitSuccess ? '접수 성공' : '접수 실패'}
-            </DialogTitle>
-            <p>
-              {submitSuccess
-                ? '배송이 정상 접수되었습니다.'
-                : '문제가 발생했습니다. 다시 시도해주세요.'}
+          {(['sender', 'receiver'] as const).map((type) => (
+            <div key={type}>
+              <h2 className="font-semibold">
+                {type === 'sender' ? '출발지 정보' : '도착지 정보'}
+              </h2>
+              <Input
+                {...register(`${type}.name`)}
+                placeholder="이름"
+                className="mt-2"
+              />
+              <Input
+                {...register(`${type}.phone`)}
+                placeholder="전화번호"
+                className="mt-2"
+              />
+              <Input
+                {...register(`${type}.address`)}
+                placeholder="주소"
+                className="mt-2"
+              />
+            </div>
+          ))}
+          <div className="flex justify-between gap-2">
+            <h2 className="font-semibold">예상 배송비</h2>
+            <p className="text-lg font-bold">
+              {`${estimatedPrice ? estimatedPrice.toLocaleString() : 0}원`}
             </p>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </FormProvider>
+          </div>
+          <div className="flex justify-between gap-2">
+            <Button type="submit" size="default" disabled={!formState.isValid}>
+              접수하기
+            </Button>
+            <div className="flex gap-2">
+              <Button onClick={onSaveData}>저장</Button>
+              <Button
+                type="button"
+                variant="secondary"
+                size="default"
+                onClick={onLoadSavedData}
+              >
+                접수 내용 불러오기
+              </Button>
+            </div>
+          </div>
+        </form>
+
+        <Dialog open={modalOpen} onOpenChange={setModalOpen}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>
+                {submitSuccess ? '접수 성공' : '접수 실패'}
+              </DialogTitle>
+              <p>
+                {submitSuccess
+                  ? '배송이 정상 접수되었습니다.'
+                  : '문제가 발생했습니다. 다시 시도해주세요.'}
+              </p>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </FormProvider>
+    </main>
   );
 }
